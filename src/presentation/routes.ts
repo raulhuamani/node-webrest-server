c9b3b809cdbaf6b8e762d8c6987b90753ffb2808
raulhuamani <raul.huamani.h@gmail.com>
2024-02-05 23:57:42 -0500

    S16 - Rest-Clean Architecture - Use Case y uso de repositorio en controladores

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { TodosController } from "./todos/controller";
 import { TodoRoutes } from "./todos/routes";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
-    const todoController = new TodosController();
 
     // router.get("/api/todos", (req, res) => TodoRoutes.routes(req, res));
     router.use("/api/todos", TodoRoutes.routes);
---
+++ src/presentation/todos/controller.ddd.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
+import { TodoRepository } from "../../domain";
+
+export class TodosController {
+  //* DI
+  constructor(private readonly todoRepository: TodoRepository) {}
+
+  public getTodos = async (req: Request, res: Response) => {
+    const todos = await this.todoRepository.getAll();
+    return res.json(todos);
+  };
+
+  public getTodoById = async (req: Request, res: Response) => {
+    const id = +req.params.id;
+
+    try {
+      const todo = await this.todoRepository.findById(id);
+      return res.json(todo);
+    } catch (error) {
+      return res.status(404).json({ error });
+    }
+  };
+
+  public createTodo = async (req: Request, res: Response) => {
+    const [error, createTodoDto] = CreateTodoDto.create(req.body);
+    if (error) return res.status(400).json({ error });
+
+    const todo = await this.todoRepository.create(createTodoDto!);
+    return res.json(todo);
+  };
+
+  public updateTodo = async (req: Request, res: Response) => {
+    const id = +req.params.id;
+    const [error, updateTodoDto] = UpdateTodoDto.create({ ...req.body, id });
+    if (error) return res.status(400).json({ error });
+
+    const updatedTodo = await this.todoRepository.updateById(updateTodoDto!);
+    return res.json(updatedTodo);
+  };
+
+  public deleteTodo = async (req: Request, res: Response) => {
+    const id = +req.params.id;
+    const deletedTodo = await this.todoRepository.deleteById(id);
+    return res.json(deletedTodo);
+  };
+}
